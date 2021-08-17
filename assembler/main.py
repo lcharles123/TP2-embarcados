0d9af15e05dc090ac1dffefa8ed8441daa828ccc
lcharles123 <root@master>
2021-08-16 23:51:48 -0300

    modified:   assembler/main.py

--- assembler/main.py
+++ assembler/main.py
@@ -11,21 +11,21 @@ instrucao = {  #  intervalos dos operandos, colocar na segunda linha, [0,3]-> re
 "multiply": '00111',  # [0,3] [0,3]
 "divide": '01000',    # [0,3] [0,3]
 "jump": '01001',      # [0,127]
-"jmpz": '01010',     # [0,3] [0,127]
-"jmpn": '01011',     # [0,3] [0,127]
-"move": '01100',     # [0,3] [0,3]
-"push": '01101',     # [0,3]
-"pop": '01110',      # [0,3]
-"call": '01111',     # [0,127]
-"return": '10000',# 0x8000
-"load_s": '10001',   # [0,3] [0,255]
-"store_s": '10010',  # [0,3] [0,255]
+"jmpz": '01010',      # [0,3] [0,127]
+"jmpn": '01011',      # [0,3] [0,127]
+"move": '01100',      # [0,3] [0,3]
+"push": '01101',      # [0,3]
+"pop": '01110',       # [0,3]
+"call": '01111',      # [0,127]
+"return": '10000',    # 0x8000
+"load_s": '10001',    # [0,3] [0,255]
+"store_s": '10010',   # [0,3] [0,255]
 "load_c": '10011',    # [0,3] imm(9bits)
 "load_i": '10100',    # [0,3] [0,3]
 "store_i": '10101',   # [0,3] [0,3]
-"copytop": '10110',  # [0,3]
-".data": '00000',  # [0,3]
-"x" : '00000'        # caso default: dont care
+"copytop": '10110',   # [0,3]
+".data": '00000',     # [0,3]
+"x" : '00000'         # caso default: dont care
 }
 
 reg = {
@@ -36,8 +36,7 @@ reg = {
 "x" : '00' # caso default: dont care
 }
 
-#recebe uma tupla com a instrucao + operadores e retorna uma tupla com a linha atual a proxima do codigo de maquina
-# formato das instrucoes:[inst][op1] + [op2]
+'''recebe a instrucao e operadores e retorna 2 elementos: a linha atual a proxima do codigo de maquina'''
 def traduzir(linha):
     inst = linha[0]
     op1 = linha[1]
@@ -54,8 +53,6 @@ def traduzir(linha):
         linha2 = b(int(op2) & 0xffff)[8:16]
         if op1 == 2: # tamanho do dado
             linha1 = b(int(op2) & 0xffff)[0:8]
-        elif op1 == 1:
-            linha1 = '00000000' # se o dado possui apenas 1 byte
         else:
             print(inst,op1,op2)
             raise ValueError("Tamanho do dado != 1 ou 2 bytes")
@@ -63,7 +60,7 @@ def traduzir(linha):
         linha1 += reg[op1] + '0' # concatena com end registrador + 0 do end memoria, possivel pois so existem 127 pos na memoria
         linha2 = b(op2)[8:16]
     elif inst in ["read", "write", "push", "pop", "copytop"]: # formato [0,3] # [0000_0][xx][x xxxx_xx00]
-        linha1 += '000' # concat para formar linha1
+        linha1 += '000'
         linha2 = '000000'+ reg[op1]
     elif inst in ["add", "subtract", "multiply", "divide", "move", "load_i", "store_i"]: # formato [0,3] [0,3] # [0000_0][00][x xxxx_xx00]
         linha1 += reg[op1] + '0'
@@ -73,35 +70,23 @@ def traduzir(linha):
         linha2 = b(op1)[8:16]
     elif inst in ["load_s", "store_s"]: # formato [0,3] [0,255] # [0000_0][00][0 0000_0000]
         linha1 += reg[op1] + '0'
-        linha2 = '1' # TODO
+        linha2 = '1'
     elif inst in ["load_c"]: # formato [0,3] imm(9bits) # [0000_0][00][0 0000_0000]
         bsinal = '0'
         if int(op2) < 0 :
             bsinal = '1'
         linha1 += reg[op1] + bsinal # este eh o bit de sinal, caso o imm(9bits) seja < 0
         linha2 = b(op2)[8:16]
-    elif inst in ["return"]: # formato 0x8000 # [xxxx_xxxx][xxxx_xxxx] 
+    elif inst in ["return", "stop"]: # formato 0x8000 # [0000_0][xxx][xxxx_xxxx] 
         linha1 += '000'
-        linha2 = '00000000' # TODO
-    elif inst in ["stop"]: # formato 0 # [xxxx_xxxx][xxxx_xxxx]
-        linha1 += '000'
-        linha2 = '00000000' # TODO
+        linha2 = '00000000'
     else:
         print(linha)
         raise ValueError("Instrucao ilegal")
-        
-    
     
     return int(linha1, 2) , int(linha2, 2)
-    return linha1 , linha2 #int(linha2, 2)
 
-#  :0100010024DA 
-#  cada linha = 8 bits (1 byte) == na memoria, duas linhas para formar uma instrucao. int possui 2 bytes = 16 bits
-# inicio + numbytes + end + tipo              +    dado + cks
-#          sempre 01        00(data), 01(EOF)
-#   :        01      0001    00                     24    DA 
-#                                                  soma = 26
-#   receber os dados como uma lista de 128 posicoes, em formato int, mas pode ser string tb
+'''Recebe um vetor de ints que serao tradizudos para o codigo de maquina'''
 def imprimir(dados, arquivo='stdout', formato='hex'):
     
     if formato == 'hex':  
@@ -118,9 +103,7 @@ def imprimir(dados, arquivo='stdout', formato='hex'):
         else:
             with open(arquivo, 'w') as f:
                 for linha in linhas:
-                    print(linha, file=f, end="")
-                    print("")
-                print("", end="")
+                    print(linha, file=f)
                 
     else:
         raise NotImplemented('mif')
@@ -136,7 +119,6 @@ def main():
         '''Forma de uso: python3 main.py asm_Swombat.a arqsaida(.hex)'''
         exit(1)
     
-    formato = "hex"
     saida = sys.argv[2]
     codMaquina = [0] * 127 # linhas do codigo de maquina
     numLinha = 0 # ennumerar as linhas do codigo para substituir pelos labels
@@ -199,15 +181,15 @@ def main():
             print(inst)
             raise ValueError("Instrucao fora do padrao")
     
-    print(codMaquina) 
-    exit(0)
+    #print(codMaquina) 
+    #exit(0)
     #h = lambda i : "{:02X}".format(i)[-2:] 
     #for i in codMaquina:
     #    print(i)
     #exit(0)
     
     #codMaquina = [10,36,12] #cada entrada representa uma linha no arquivo hex/mif
-    #imprimir(codMaquina, arquivo=saida)
+    imprimir(codMaquina, arquivo=saida)
    
    
 
